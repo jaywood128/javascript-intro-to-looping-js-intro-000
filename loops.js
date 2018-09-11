7ba6173ae9966fb5de94586f58d5abb4af606b9b
Johnathon Wood <jaywood128@gmail.com>
2018-09-11 17:02:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,15 +18,15 @@ function whileLoop(n) {
 }
 function doWhileLoop(array) {
 	var i = 0;
- 
+
   function incrementVariable() {
     i = i + 1;
 }
 
 do {
-	  console.log('array.length = ' + array.length + ' and i = ' + i); 
+	  console.log('array.length = ' + array.length + ' and i = ' + i);
 	  array = array.slice(1);
-		incrementVariable(); 
-} while (array.length > 0 && i < 5); 
+		incrementVariable();
+} while (array.length > 0 && i < 5);
 
 return array; 
